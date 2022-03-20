fdd3b0e2300de09c095bade2eed59ca0fd97a6a8
yarman1 <gordienko045@gmail.com>
2022-03-20 12:08:09 +0200

    created field

--- tetris.js
+++ tetris.js
@@ -1,6 +1,20 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const field = [];
+const FIELD_SIZE = {
+  height: 22,
+  width: 10,
+}
+
+for (let i = 0; i < FIELD_SIZE.height; i++) {
+  field[i] = [];
+  for (let j = 0; j < FIELD_SIZE.width; j++) {
+    field[i][j] = 0;
+  }
+}
+console.dir(field);
+
 const FIGURES = {
   J: [
       [0, 1, 0],
